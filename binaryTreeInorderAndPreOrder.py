834273b4876a64793937fb40c69c80c676bdc086
HAORAN WEN <hwen@apple.com>
2019-02-26 19:18:08 -0800

    more

---
+++ binaryTreeInorderAndPreOrder.py
@@ -0,0 +1,58 @@
+https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+
+Given preorder and inorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+
+For example, given
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+preoder: root, left, right
+inorder: left, root, right
+
+[root, left1,left2,left3,right1,right2]
+[left1,left2, left3, root, right1, right2]
+
+when we get a node from preorder, we treat as a current root, we find that value
+in inorder travel, so the left part length of inorder travel equals the left part 
+nodes in preorder travel, we recursively send the left part and right part
+
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        #we hash the inorder values
+        vmap = {}
+        for i in range(len(inorder)):
+            vmap[inorder[i]] = i
+
+        def helper(preorder, inorder):
+            #base case
+            if not preorder or not inorder:
+                return None
+            currentRootValue = preorder[0]
+            #find matching index, careful we need the relative rootIndex as the inorder
+            #list is not the original one
+            originalRootIndex = vmap[currentRootValue]
+            relativeRootIndex = originalRootIndex - vmap[inorder[0]]
+
+            currentRoot = TreeNode(currentRootValue)
+            leftNode = helper(preorder[1:relativeRootIndex + 1], inorder[:relativeRootIndex])
+            rightNode = helper(preorder[relativeRootIndex + 1:], inorder[relativeRootIndex + 1:])
+            currentRoot.left = leftNode
+            currentRoot.right = rightNode
+            return currentRoot
+        return helper(preorder, inorder)
+
+
+
+
+        
---
+++ missingNumber.py
@@ -0,0 +1,9 @@
+https://leetcode.com/problems/missing-number/
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        expectedSum =  n * (n + 1) // 2
+        actualSum = sum(nums)
+        return expectedSum - actualSum
