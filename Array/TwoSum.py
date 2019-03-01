dc2fe98a685bea84ed848f8cb7fca9c602dd9ae2
harrywenhro <harrywenhr@gmail.com>
2019-02-28 22:10:13 -0800

    more

---
+++ Array/TwoSum.py
@@ -0,0 +1,15 @@
+https://leetcode.com/problems/two-sum/submissions/
+class Solution(object):
+    def twoSum(self, nums, target):
+        valueDic = {}
+        for idx, num in enumerate(nums):
+            if (target - num) in valueDic:
+                return [valueDic[(target - num)], idx]
+            #set destiny pair index
+            valueDic[num] = idx
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
